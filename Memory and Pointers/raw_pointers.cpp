1e68cd2cc8d3f7b3c9686a2037a90561adf88799
kasualkid12 <kasu@kasu-it.dev>
2025-04-15 01:29:00 -0500

    added basic i/o functionality

--- Memory and Pointers/raw_pointers.cpp
+++ Memory and Pointers/raw_pointers.cpp
@@ -16,3 +16,27 @@
 // Step 2: Ensure the logic is the same as in the previous part. without explicitly calling delete.
 // Step 3: Observe how the smart pointer automatically manages memory.
 
+
+// user should be prompted to enter n, a number signifying the amount of integers going to be inputed to an array
+// the user should then be prompted to input said numbers either one-by-one, or all in one line seperated by spaces
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // create int to store array size
+  int x;
+  cout << "Input the amount of numbers you want to calculate together: ";
+  // get array size from user
+  cin >> x;
+  cout << "An array of " << x << " size was created!" << "\n";
+
+  for (int i = 0; i < x; i++){
+    int y;
+    cout << "Input number " << i + 1 << ": ";
+    cin >> y;
+    cout << "Your number, " << y << " was inputed to the array." << "\n";
+  }
+
+}
